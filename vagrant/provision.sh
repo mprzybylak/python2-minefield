7f3e4fa9364da081feba46ba675251879f416b11
Michal Przybylak <michal.przybylak@gmail.com>
2014-04-08 22:43:18 +0200

    Initial Vagrant configuration with installation of python 3.4

---
+++ vagrant/provision.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "Installing Python..."
+#apt-get update
+#apt-get install -y -q python-software-properties
+#add-apt-repository -y ppa:fkrull/deadsnakes
+#apt-get update
+#apt-get install python3.4
